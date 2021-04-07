43cbf10bfed68662c89f25680df0941f508ed090
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-04-07 07:03:40 +0100

    add GroundNode SourceNode Arc

--- directedgraph/dgcore/graphelement.py
+++ directedgraph/dgcore/graphelement.py
@@ -1,4 +1,3 @@
-from collections import UserList
 import uuid
 
 
@@ -35,11 +34,14 @@ class GraphElement:
                     self.uid = new_uid
                     return new_uid
 
-    def get_parent_graph(self):
-        return self.parent_graph
-
     def get(self, a):
         return vars(self).get(a, None)
+
+    def get_all(self):
+        return vars(self)
+
+    def get_parent_graph(self):
+        return self.parent_graph
 
     def get_uid(self):
         return self.uid
@@ -49,9 +51,6 @@ class GraphElement:
 
     def get_colour(self):
         return self.colour
-
-    def get_all(self):
-        return vars(self)
 
 
 class Node(GraphElement):
@@ -74,11 +73,57 @@ class Node(GraphElement):
         self.position[1] = position_y
 
 
+class GroundNode(Node):
+    def __init__(
+        self,
+        parent_graph=None,
+        uid=None,
+        name=None,
+        colour=None,
+        position=None,
+    ):
+        super().__init__(parent_graph, uid, name, colour, position)
+        self.user_defined_attribute = "0"
+
+
+class SourceNode(Node):
+    def __init__(
+        self,
+        parent_graph=None,
+        uid=None,
+        name=None,
+        colour=None,
+        position=None,
+        user_defined_attribute=None,
+    ):
+        super().__init__(parent_graph, uid, name, colour, position)
+        self.user_defined_attribute = (
+            "0" if user_defined_attribute == None else user_defined_attribute
+        )
+
+
+class Arc(GraphElement):
+    def __init__(
+        self,
+        parent_graph=None,
+        uid=None,
+        name=None,
+        colour=None,
+    ):
+        super().__init__(parent_graph, uid, name, colour)
+
+
 if __name__ == "__main__":
-    a = GraphElement()
-    print(vars(a))
-    print(a.get("name"))
-    # b = Node(None, None, None, None, [1, 2])
-    b = Node()
-    print(b.get_all())
-    print(b.get("position"))
+    element1 = GraphElement()
+    print(vars(element1))
+    print(element1.get("name"))
+    # node1 = Node(None, None, None, None, [1, 2])
+    node1 = Node()
+    print(node1.get_all())
+    node1.update_node_position(0, 9)
+    print(node1.get("position"))
+    print(node1.get_position())
+    groundnode1 = GroundNode()
+    print(groundnode1.get_all())
+    sourcenode1 = SourceNode(None, None, "sourcenode1", None, [2, 3], "abc")
+    print(sourcenode1.get_all())
