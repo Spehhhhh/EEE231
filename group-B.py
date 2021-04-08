908579467bea68fc59f3331b90161acea58ee116
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-04-08 20:54:55 +0100

    style

--- group-B.py
+++ group-B.py
@@ -11,14 +11,27 @@
 from sys import exit
 
 from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QHBoxLayout,
+    QVBoxLayout,
+    QWidget,
+)
 from PySide6.QtWidgets import QMenuBar, QMenu
 from PySide6.QtWidgets import QToolBar, QStatusBar
 from PySide6.QtWidgets import QFileDialog, QMessageBox
 
 from PySide6.QtCore import Qt, QPointF, QRectF
 from PySide6.QtGui import QColor, QPainter, QPainterPath, QPen, QBrush
-from PySide6.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsEllipseItem, QGraphicsPathItem, QGraphicsSimpleTextItem, QGraphicsItemGroup
+from PySide6.QtWidgets import (
+    QGraphicsScene,
+    QGraphicsView,
+    QGraphicsEllipseItem,
+    QGraphicsPathItem,
+    QGraphicsSimpleTextItem,
+    QGraphicsItemGroup,
+)
 
 # ******************************************************************************
 
@@ -33,8 +46,7 @@ class MainWindow(QMainWindow):
         # Setup menu bar & File menu
         self.fileMenu = self.menuBar().addMenu("&File")
         self.openMenuAction = self.fileMenu.addAction("&Open")
-        self.openMenuAction.triggered.connect(
-            self.on_open_action)    # New-style connect!
+        self.openMenuAction.triggered.connect(self.on_open_action)  # New-style connect!
         self.fileMenu.addSeparator()
         self.quitMenuAction = self.fileMenu.addAction("&Quit")
         self.quitMenuAction.triggered.connect(self.on_quit_action)
@@ -42,8 +54,7 @@ class MainWindow(QMainWindow):
         # Setup Tools menu
         self.toolsMenu = self.menuBar().addMenu("&Tools")
         self.preferencesMenuAction = self.toolsMenu.addAction("&Preferences")
-        self.preferencesMenuAction.triggered.connect(
-            self.on_preferences_action)
+        self.preferencesMenuAction.triggered.connect(self.on_preferences_action)
 
         # Setup About menu
         self.aboutMenu = self.menuBar().addMenu("&About")
@@ -75,8 +86,12 @@ class MainWindow(QMainWindow):
         y1 = 100.0
         self.nodeRadius = 25.0
         nodeCentre_1 = QPointF(x1, y1)
-        rect_1 = QRectF(x1 - self.nodeRadius, y1 - self.nodeRadius, 2.0 * self.nodeRadius,
-                        2.0 * self.nodeRadius)  # Bounding rectangle of node 'ellipse'
+        rect_1 = QRectF(
+            x1 - self.nodeRadius,
+            y1 - self.nodeRadius,
+            2.0 * self.nodeRadius,
+            2.0 * self.nodeRadius,
+        )  # Bounding rectangle of node 'ellipse'
         # NB. Parameter defines the bounding rectangle of the ellipse!
         nodeGraphicsItem_1 = QGraphicsEllipseItem(rect_1)
         nodeGraphicsItem_1.setZValue(0)
@@ -85,8 +100,10 @@ class MainWindow(QMainWindow):
 
         nodeTextItem_1 = QGraphicsSimpleTextItem("Node 1")
         boundingRect_1 = nodeTextItem_1.boundingRect()
-        nodeTextItem_1.setPos(nodeCentre_1.x(
-        ) - boundingRect_1.width()/2, nodeCentre_1.y() - boundingRect_1.height() / 2)
+        nodeTextItem_1.setPos(
+            nodeCentre_1.x() - boundingRect_1.width() / 2,
+            nodeCentre_1.y() - boundingRect_1.height() / 2,
+        )
         nodeGraphicsGroup_1 = QGraphicsItemGroup()
         nodeGraphicsGroup_1.addToGroup(nodeGraphicsItem_1)
         nodeGraphicsGroup_1.addToGroup(nodeTextItem_1)
@@ -96,8 +113,12 @@ class MainWindow(QMainWindow):
         x2 = 100.0
         y2 = 200.0
         nodeCentre_2 = QPointF(x2, y2)
-        rect_2 = QRectF(x2 - self.nodeRadius, y2 - self.nodeRadius,
-                        2.0 * self.nodeRadius, 2.0 * self.nodeRadius)
+        rect_2 = QRectF(
+            x2 - self.nodeRadius,
+            y2 - self.nodeRadius,
+            2.0 * self.nodeRadius,
+            2.0 * self.nodeRadius,
+        )
         nodeGraphicsItem_2 = QGraphicsEllipseItem(rect_2)
         nodeGraphicsItem_2.setZValue(0)
         nodeGraphicsItem_2.setBrush(nodeFillBrush)
@@ -105,8 +126,10 @@ class MainWindow(QMainWindow):
 
         nodeTextItem_2 = QGraphicsSimpleTextItem("Node 2")
         boundingRect_2 = nodeTextItem_1.boundingRect()
-        nodeTextItem_2.setPos(nodeCentre_2.x(
-        ) - boundingRect_2.width()/2, nodeCentre_2.y() - boundingRect_2.height() / 2)
+        nodeTextItem_2.setPos(
+            nodeCentre_2.x() - boundingRect_2.width() / 2,
+            nodeCentre_2.y() - boundingRect_2.height() / 2,
+        )
         nodeGraphicsGroup_2 = QGraphicsItemGroup()
         nodeGraphicsGroup_2.addToGroup(nodeGraphicsItem_2)
         nodeGraphicsGroup_2.addToGroup(nodeTextItem_2)
@@ -116,8 +139,7 @@ class MainWindow(QMainWindow):
         arc_PainterPath_1 = QPainterPath(nodeCentre_1)
         ctrlX_1 = 50.0
         ctrlY_1 = 150.0
-        arc_PainterPath_1.quadTo(
-            ctrlX_1, ctrlY_1, nodeCentre_2.x(), nodeCentre_2.y())
+        arc_PainterPath_1.quadTo(ctrlX_1, ctrlY_1, nodeCentre_2.x(), nodeCentre_2.y())
         arcGraphicItem_1 = QGraphicsPathItem(arc_PainterPath_1)
         arcGraphicItem_1.setZValue(-1)  # Hides path below nodes
         arcGraphicItem_1.setToolTip("arc 1")
@@ -127,8 +149,7 @@ class MainWindow(QMainWindow):
         arc_PainterPath_2 = QPainterPath(nodeCentre_1)
         ctrlX_2 = 150.0
         ctrlY_2 = 150.0
-        arc_PainterPath_2.quadTo(
-            ctrlX_2, ctrlY_2, nodeCentre_2.x(), nodeCentre_2.y())
+        arc_PainterPath_2.quadTo(ctrlX_2, ctrlY_2, nodeCentre_2.x(), nodeCentre_2.y())
         arcGraphicItem_2 = QGraphicsPathItem(arc_PainterPath_2)
         arcGraphicItem_2.setZValue(-1)  # Hides path below nodes
         arcGraphicItem_2.setToolTip("arc 2")
@@ -137,8 +158,7 @@ class MainWindow(QMainWindow):
         # Create graphics view
         self.graphicsView = QGraphicsView(self.graphicsScene)
         self.graphicsView.setDragMode(QGraphicsView.RubberBandDrag)
-        self.graphicsView.setRubberBandSelectionMode(
-            Qt.ContainsItemBoundingRect)
+        self.graphicsView.setRubberBandSelectionMode(Qt.ContainsItemBoundingRect)
         self.graphicsView.setRenderHints(QPainter.Antialiasing)
         self.mainLayout.addWidget(self.graphicsView)
 
@@ -155,8 +175,7 @@ class MainWindow(QMainWindow):
 
     def on_open_action(self):
         """Handler for 'Open' action"""
-        fileName = QFileDialog.getOpenFileName(
-            self, "Open File", ".", ("*.md"))
+        fileName = QFileDialog.getOpenFileName(self, "Open File", ".", ("*.md"))
         print("opening ", fileName[0])
         return
 
@@ -167,6 +186,7 @@ class MainWindow(QMainWindow):
         print("quitting application")
         self.close()
         return
+
     # --------------------------------------------------------------------------
 
     def on_preferences_action(self):
@@ -178,9 +198,13 @@ class MainWindow(QMainWindow):
 
     def on_about_action(self):
         """Handler for 'About' action"""
-        QMessageBox.about(self, "About this program",
-                          "some about text crediting the people who wrote this")
+        QMessageBox.about(
+            self,
+            "About this program",
+            "some about text crediting the people who wrote this",
+        )
         return
+
 
 # ******************************************************************************
 
