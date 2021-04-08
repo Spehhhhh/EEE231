691e3763a8f4b779581a1d7f01893f9fd998daab
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-04-08 20:55:28 +0100

    add debug log

--- tests/test_dgcore.py
+++ tests/test_dgcore.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from loguru import logger
 
 current_folder = Path(__file__).absolute().parent
 father_folder = str(current_folder.parent)
@@ -7,5 +8,23 @@ sys.path.append(father_folder)
 
 import directedgraph
 
-MyGraph = directedgraph.dgcore.Graph("MyGraph")
-MyGraph.print_graph_details()
+logger.add(
+    "logs/test_dgcore.py.log",
+    level="DEBUG",
+    format="{time:YYYY-MM-DD :mm:ss} - {level} - {file} - {line} - {message}",
+    rotation="10 MB",
+)
+logger.info("Start Log")
+
+
+@logger.catch
+def test_id():
+    graph1 = directedgraph.dgcore.Graph("graph1")
+    graph2 = directedgraph.dgcore.Graph("graph2")
+    graph1.create_element({"type": "Node", "name": "No UID 1"})
+    graph1.create_element({"type": "Node", "name": "Foo", "uid": "7778da0a0a0a"})
+    graph1.print_graph_details()
+    graph2.print_graph_details()
+
+
+test_id()
