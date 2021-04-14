e6e2cca4edb12b198d348a8d02afbc36a016fd71
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-04-14 23:41:37 +0100

    add test_dgcore unittest

--- tests/test_dgcore.py
+++ tests/test_dgcore.py
@@ -1,5 +1,8 @@
 import sys
+import time
 import timeit
+import itertools
+import unittest
 from pathlib import Path
 from loguru import logger
 
@@ -7,7 +10,15 @@ current_folder = Path(__file__).absolute().parent
 father_folder = str(current_folder.parent)
 sys.path.append(father_folder)
 
-from directedgraph.dgcore import Graph, Node, create_graph
+from directedgraph.dgcore import (
+    GraphElement,
+    Graph,
+    Node,
+    GroundNode,
+    SourceNode,
+    Arc,
+    create_graph,
+)
 
 logger.add(
     "logs/test_dgcore.py.log",
@@ -18,89 +29,101 @@ logger.add(
 logger.info("Start Log")
 
 
-@logger.catch
-def test_id():
-    graph1 = Graph("graph1")
-    graph2 = Graph("graph2")
-    graph1.create_element({"type": "Node", "name": "No UID 1"})
-    graph1.create_element({"type": "Node", "name": "Foo", "uid": "7778da0a0a0a"})
-    graph1.print_graph_details()
-    graph2.print_graph_details()
+class GraphUsage(unittest.TestCase):
+    @logger.catch
+    def test_id(self):
+        graph1 = Graph("graph1")
+        graph1.create_element({"type": "Node", "name": "Node without UID"})
+        graph1.create_element({"type": "Node", "name": "Node 1", "uid": "7778da0a0a0a"})
 
+        graph2 = Graph("graph2")
 
-@logger.catch
-def test_insert_element():
-    my_graph = Graph("MyGraph")
-    node1 = Node(my_graph, "b911b214f553", "Node1")
-    node2 = Node(my_graph, "9a2812943a39", "Node2")
-    my_graph.insert_element(node1)
-    my_graph.insert_element(node2)
-    my_graph.print_graph_details()
-    print(my_graph.get_element("9a2812943a39"))
-    print(my_graph)
-    print(my_graph.elements["9a2812943a39"].parent_graph)
+        self.assertEqual(graph2.elements, {})
+        self.assertEqual(len(graph1.elements), 2)
 
+    @logger.catch
+    def test_insert_element(self):
+        graph1 = Graph("graph1")
 
-@logger.catch
-def test_create_element():
-    my_graph = Graph("MyGraph")
-    my_graph.create_element({"type": "Node", "name": "Node1", "uid": "7778da0a0a0a"})
-    my_graph.print_graph_details()
+        node1 = Node(graph1, "b911b214f553", "node1")
+        graph1.insert_element(node1)
+        node2 = Node(graph1, "9a2812943a39", "node2")
+        graph1.insert_element(node2)
 
+        self.assertEqual(len(graph1.elements), 2)
+        self.assertEqual(graph1.get_element("b911b214f553"), node1)
+        self.assertEqual(graph1.get_element("b911b214f553").name, "node1")
+        self.assertEqual(graph1.get_element("9a2812943a39").parent_graph, graph1)
 
-@logger.catch
-def test_query_and_delete():
-    print("---Init---")
-    my_graph = Graph("MyGraph")
-    my_graph.create_element({"type": "Node", "name": "No UID 1"})
-    my_graph.create_element({"type": "Node", "name": "No UID 2"})
-    my_graph.create_element({"type": "Node", "name": "Fooo", "uid": "7778da0a0a0a"})
-    my_graph.create_element({"type": "Node", "name": "Foooo", "uid": "32a24bfcfefe"})
-    my_graph.print_graph_details()
-    print("---Try Get Node---")
-    print(my_graph.get_element("7778da0a0a0a"))
-    print(my_graph.get_element("32a24bfcfefe"))
-    print("---Try Delete---")
-    print(my_graph.delete_element("32a24bfcfefe"))
-    print(my_graph.delete_element("31233"))
-    my_graph.print_graph_details()
-    print(my_graph)
-    print("---Try Node---")
-    node1 = Node(my_graph, "b911b4f553")
-    print(node1.get_parent_graph())
+    @logger.catch
+    def test_create_element(self):
+        graph1 = Graph("graph1")
+        graph1.create_element({"type": "Node", "name": "node1", "uid": "b911b214f553"})
 
+        self.assertEqual(graph1.get_element("b911b214f553").name, "node1")
+        self.assertEqual(type(graph1.get_element("b911b214f553")), Node)
 
-@logger.catch
-def test_duplicate_key():
-    my_graph = Graph("MyGraph")
-    my_graph.create_element({"type": "Node", "name": "No UID 1"})
-    my_graph.create_element({"type": "Node", "name": "No UID 2"})
-    my_graph.create_element({"type": "Node", "name": "Foo", "uid": "7778da0a0a0a"})
-    my_graph.create_element({"type": "Node", "name": "Fooo", "uid": "7778da0a0a0a"})
-    my_graph.create_element({"type": "Arc", "name": "Fooo", "uid": "7778da0a0a0a"})
-    my_graph.create_element({"name": "Fooo", "uid": "7778da0a0a0a"})
-    my_graph.print_graph_details()
-    # MyGraph.get_element("7778da0a0a0a")
+    @logger.catch
+    def test_create_graph(self):
+        name = "graph1"
+        data = [
+            {"type": "Node", "name": "node1", "uid": "7778da0a0a0a"},
+            {"type": "Node", "name": "node2", "uid": "32a24bfcfefe"},
+            {"type": "Node", "uid": "32a24bfcfefe"},
+        ]
+        graph1 = create_graph(name, data)
 
+        self.assertEqual(graph1.name, "graph1")
+        self.assertEqual(graph1.get_element("32a24bfcfefe").name, "node2")
+        self.assertEqual(len(graph1.elements), 3)
 
-@logger.catch
-def test_create_graph():
-    name = "my_graph"
-    data = [
-        {"type": "Node", "name": "Node1", "uid": "7778da0a0a0a"},
-        {"type": "Node", "name": "Node2", "uid": "32a24bfcfefe"},
-        {"type": "Node", "name": "Node2", "uid": "32a24bfcfefe"},
-        {"type": "Node", "uid": "32a24bfcfefe"},
-    ]
-    my_graph = create_graph(name, data)
-    my_graph.print_graph_details()
+    @logger.catch
+    def test_generate_element_uid(self):
+        graph1 = Graph("graph1")
+        graph1.create_element({"type": "Node", "name": "Node without UID 1"})
+        graph1.create_element({"type": "Node", "name": "Node without UID 2"})
+        graph1.create_element(
+            {"type": "Node", "name": "Node with UID", "uid": "7778da0a0a0a"}
+        )
+        graph1.create_element(
+            {"type": "Node", "name": "Node with duplicate UID", "uid": "7778da0a0a0a"}
+        )
+
+        self.assertEqual(len(graph1.elements), 4)
+
+    @logger.catch
+    def test_error(self):
+        graph1 = Graph("graph1")
+        # graph1.create_element({"type": "Arc", "name": "Arc 1", "uid": "7778da0a0a0a"})
+        # graph1.create_element({"name": "Fooo", "uid": "7778da0a0a0a"})
+
+    @logger.catch
+    def test_query_and_delete(self):
+        # print("---Init---")
+        graph1 = Graph("graph1")
+        graph1.create_element({"type": "Node", "name": "Node without UID 1"})
+        graph1.create_element({"type": "Node", "name": "Node without UID 2"})
+        graph1.create_element({"type": "Node", "name": "Foo", "uid": "7778da0a0a0a"})
+        graph1.create_element({"type": "Node", "uid": "32a24bfcfefe"})
+        self.assertEqual(len(graph1.elements), 4)
+
+        # print("---Try Get Node---")
+        self.assertEqual(graph1.get_element("7778da0a0a0a").name, "Foo")
+        self.assertEqual(graph1.get_element("32a24bfcfefe").name, "Untitled")
+
+        # print("---Try Delete---")
+        self.assertEqual(graph1.delete_element("32a24bfcfefe"), True)
+        self.assertEqual(graph1.delete_element("32a24bfcfefe"), False)
+        self.assertEqual(graph1.delete_element("31233"), False)
+        self.assertEqual(len(graph1.elements), 3)
+
+    def test_graph_efficiency(self):
+        start_time = time.time()
+        for _ in itertools.repeat(None, 100):
+            self.test_query_and_delete()
+        end_time = time.time()
+        print("Elapsed time was %g seconds" % (end_time - start_time))
 
 
 if __name__ == "__main__":
-    print(
-        timeit.timeit(
-            "test_create_graph()",
-            setup="from __main__ import test_create_graph",
-            number=1,
-        )
-    )
+    unittest.main()
