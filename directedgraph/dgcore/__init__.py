c0ff23b8726ff576ab3c44c6e78d9fdbb6cb7a57
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-04-20 00:22:18 +0100

    fix init

--- directedgraph/dgcore/__init__.py
+++ directedgraph/dgcore/__init__.py
@@ -2,11 +2,12 @@ from pathlib import Path
 
 print("Running" if __name__ == "__main__" else "Importing", Path(__file__).resolve())
 
+from directedgraph.dgcore.excp import GroundNodeNumberException
 from directedgraph.dgcore.graphelement import (
     GraphElement,
     Node,
-    GroundNode,
     SourceNode,
+    GroundNode,
     Arc,
 )
 from directedgraph.dgcore.graph import Graph
