81c2b10c68fd8a66b59d6ec277330726544dc10f
lyb1234567 <81267855+lyb1234567@users.noreply.github.com>
2021-05-02 09:12:30 +0100

    add test case for get_function method from Arc class

--- tests/test_dgcore_graphelement.py
+++ tests/test_dgcore_graphelement.py
@@ -39,7 +39,7 @@ def test_groundnode():
     sourcenode1 = SourceNode(None, None, "sourcenode1", None, [2, 3], "abc")
     print(sourcenode1.get())
 
-
+# pass  node as uid
 def test_arc_init_case_1():
     from directedgraph.dgcore.graph import Graph
 
@@ -59,7 +59,7 @@ def test_arc_init_case_1():
     # print(arc1.nodes)
     print(arc1.get_position())
 
-
+# pass node as objects
 def test_arc_init_case_2():
     node1 = Node(None, None, "node1", None, None)
     node2 = GroundNode(None, None, "node2", None, [10, 10])
@@ -75,7 +75,33 @@ def test_arc_init_case_2():
     # print(arc1.nodes)
     print(arc1.get_position())
 
+def test_arc_function():
+    node1=GroundNode(None, None, "node1", None, None)
+    # set SourceNode user_define_attribute to 5(5v)
+    node2=SourceNode(None,None,"ndoe2",None,None,10)
+    arc1=Arc(None,"sdasd",'arc1',None,node1,node2,"resistance",5)
+    arc1.get_function()
+    print(arc1.get())
+    arc1.update_function("resistance")
+    print(arc1.get())
+
+    from directedgraph.dgcore.graph import Graph
+
+    graph1 = Graph("graph1")
+    graph1.create_element({"type": "Node", "name": "Node 1", "uid": "859e4b2ec309"})
+    graph1.create_element({"type": "Node", "name": "Node 2", "uid": "7778da0a0a0a"})
+    # graph1.print_graph_details()
+    arc2 = Arc(
+        graph1,
+        "123",
+        None,
+        "arc2",
+        "859e4b2ec309",
+        "7778da0a0a0a",
+    )
+    arc2.get_function()
 
 if __name__ == "__main__":
-    test_arc_init_case_2()
+    # test_arc_init_case_2()
+    test_arc_function()
     pass
