0082e9759687d528161bbf61a4fa33a468d2e1d6
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-05-21 01:08:22 +0100

    fix main.py

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-from sys import exit
+import sys
 from PySide6.QtWidgets import QApplication
 
 from directedgraph.dggui import DirectedGraphMainWindow
 
 
 class DirectedGraphApplication:
+    """
+    Run
+    """
+
     def __init__(self):
         self.auto_save_interval = "300s"
+        self.application = QApplication([])
+        self.mainwindow = DirectedGraphMainWindow()
 
     def run(self):
+        """
+        Run
+        """
+
         print("Running")
-        self.application = QApplication([])
-
-        self.mainwindow = DirectedGraphMainWindow()
         self.mainwindow.show()  # .showMaximized()
-
-        exit(self.application.exec_())
+        sys.exit(self.application.exec_())
 
     def run_debug(self):
+        """
+        Debug
+        """
         print("Running Debug Mode")
+        self.mainwindow.show()
+        sys.exit(self.application.exec_())
 
 
 if __name__ == "__main__":
