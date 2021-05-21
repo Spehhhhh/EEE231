a15384cf6cd5d28a44006c3d9b769e8836f4af6b
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-05-21 01:08:03 +0100

    fix Graph()

--- directedgraph/dgcore/graph.py
+++ directedgraph/dgcore/graph.py
@@ -2,9 +2,9 @@ import sys
 from pathlib import Path
 
 print("Running" if __name__ == "__main__" else "Importing", Path(__file__).resolve())
-current_folder = Path(__file__).absolute().parent.parent
-father_folder = str(current_folder.parent)
-sys.path.append(father_folder)
+CURRENT_DIRECTORY = Path(__file__).absolute()
+ROOT_FOLDER = CURRENT_DIRECTORY.parent.parent.parent
+sys.path.append(str(ROOT_FOLDER))
 
 from directedgraph.dgcore import Node, SourceNode, GroundNode, Arc
 
@@ -16,7 +16,6 @@ class Graph:
 
     def __init__(self, name=None):
         self.connected_window = None
-
         self.groundnode_counter = 0
         self.arc_counter = 0
 
@@ -28,12 +27,16 @@ class Graph:
     """
 
     def get(self):
+        """
+        #TODO
+        """
+
         graph_attribute = []
         graph_attribute.append({"name": self.name})
 
         graph_components = []
         for component in self.components.values():
-            if type(component) == Arc:
+            if isinstance(component, Arc):
                 component_dict = {}
                 component_dict["type"] = "Arc"
                 component_dict["uid"] = str(component.uid)
@@ -48,7 +51,7 @@ class Graph:
                     component.user_defined_arc_type
                 )
                 graph_components.append(component_dict)
-            elif type(component) == Node:
+            elif isinstance(component, Node):
                 component_dict = {}
                 component_dict["type"] = "Node"
                 component_dict["uid"] = str(component.uid)
@@ -56,39 +59,37 @@ class Graph:
                 component_dict["colour"] = str(component.colour)
                 component_dict["position_x"] = str(component.position[0])
                 component_dict["position_y"] = str(component.position[1])
-                graph_components.append(component_dict)
-            elif type(component) == SourceNode:
-                component_dict = {}
-                component_dict["type"] = "SourceNode"
-                component_dict["uid"] = str(component.uid)
-                component_dict["name"] = str(component.name)
-                component_dict["colour"] = str(component.colour)
-                component_dict["position_x"] = str(component.position[0])
-                component_dict["position_y"] = str(component.position[1])
-                component_dict["user_defined_attribute"] = str(
-                    component.user_defined_attribute
-                )
-                graph_components.append(component_dict)
-            elif type(component) == GroundNode:
-                component_dict = {}
-                component_dict["type"] = "GroundNode"
-                component_dict["uid"] = str(component.uid)
-                component_dict["name"] = str(component.name)
-                component_dict["colour"] = str(component.colour)
-                component_dict["position_x"] = str(component.position[0])
-                component_dict["position_y"] = str(component.position[1])
+                if isinstance(component, SourceNode):
+                    component_dict["type"] = "SourceNode"
+                    component_dict["user_defined_attribute"] = str(
+                        component.user_defined_attribute
+                    )
+                if isinstance(component, GroundNode):
+                    component_dict["type"] = "GroundNode"
                 graph_components.append(component_dict)
             else:
                 pass
         return (graph_attribute, graph_components)
 
     def get_name(self):
+        """
+        #TODO
+        """
+
         return self.name
 
     def update_name(self, name):
+        """
+        #TODO
+        """
+
         self.name = name
 
     def update_component_node_arcs(self):
+        """
+        #TODO
+        """
+
         self.arc_counter = 0
         components_values = self.components.values()
 
@@ -97,12 +98,15 @@ class Graph:
                 component_inst.arcs.clear()
 
         for component_inst in components_values:
-            if type(component_inst) == Arc:
+            if isinstance(component_inst, Arc):
                 self.arc_counter += 1
                 component_inst.nodes[0].arcs.append(component_inst)
                 component_inst.nodes[1].arcs.append(component_inst)
 
     def verify_graph_integrity(self):
+        """
+        #TODO
+        """
         return_list = []
         return_list.clear()
         components_values = self.components.values()
@@ -111,9 +115,11 @@ class Graph:
 
         # Only one Ground Node is allowed
         for component_inst in components_values:
-            if type(component_inst) == GroundNode:
+            if isinstance(component_inst, GroundNode):
                 self.groundnode_counter += 1
-        if self.groundnode_counter != 1:
+        if self.groundnode_counter == 0:
+            return_list.append("You need at least one GroundNode")
+        elif self.groundnode_counter != 1:
             return_list.append("Only one Ground Node is allowed")
         else:
             pass
@@ -122,7 +128,7 @@ class Graph:
         self.update_component_node_arcs()
 
         for component_inst in components_values:
-            if type(component_inst) == SourceNode:
+            if isinstance(component_inst, SourceNode):
                 if len(component_inst.arcs) > 1:
                     return_list.append("Source only allows single arcs")
 
@@ -131,37 +137,25 @@ class Graph:
 
         return return_list
 
-    # For debugging Graph
-    def print_graph_details(self):
-        print("------------------------------------")
-        print("Graph vars(self):")
-        print(vars(self))
-        print("------------------------------------")
-        print("Graph Components:")
-        for (
-            component_uid,
-            component,
-        ) in self.components.items():
-            print(
-                "UID:",
-                component.uid,
-                "|",
-                "Name:",
-                component.name,
-            )
-
     """
     Control GraphComponent()
     """
 
     def get_component(self, uid):
+        """
+        #TODO
+        """
+
         # print(self.components[uid].get_name())
         # print("connected_graph:", self.components[uid].get_connected_graph())
         # print(vars(self.components[uid])) # 可以返回对象也可以返回字典
         return self.components[uid]
 
-    # #TODO 按参数里的字典新建组件
     def create_component(self, parameters):
+        """
+        create component according to the dictionary in the parameters
+        """
+
         if parameters.get("type", None) == "Node":
             component = Node(
                 self,
@@ -219,35 +213,77 @@ class Graph:
             print("Error: Component Type")
 
     def insert_component(self, component):
+        """
+        #TODO
+        """
+
         self.components[component.get_uid()] = component
 
     def update_component_name(self, uid, name):
+        """
+        #TODO
+        """
+
         self.components[uid].name = name
 
     def update_component_colour(self, uid, colour):
+        """
+        #TODO
+        """
+
         self.components[uid].colour = colour
 
     # def update_arc_position(uid, uid, uid):
     # def update_arc_position(uid, node1, node2):
     # def update_arc_position(arc1, node1, node2):
     def update_arc_position(self, arc1, node1, node2):
-        if type(arc1) == Arc:
+        """
+        Query and delete components using uid
+        """
+
+        if isinstance(arc1) == Arc:
             arc1.update_position(node1, node2)
         elif isinstance(arc1, str):
             if len(node2) == 12 and self.connected_graph == self:
-                self.get_component["arc1"].update_position(node1, node2)
+                self.get_component("arc1").update_position(node1, node2)
 
-    # #TODO 需要写误删除逻辑
     def delete_component(self, uid):
+        """
+        Query and delete components using uid
+        """
+
         if uid in self.components:
             self.components.pop(uid)
             return True
         else:
             return False
 
+    """
+    For debugging Graph
+    """
+
+    def print_graph_details(self):
+        """
+        Print UID|Name
+        """
+
+        print("------------------------------------")
+        print("Graph vars(self):")
+        print(vars(self))
+        print("------------------------------------")
+        print("Graph Components:")
+        for component in self.components.values():
+            print(
+                "UID:",
+                component.uid,
+                "|",
+                "Name:",
+                component.name,
+            )
+
 
 if __name__ == "__main__":
     import unittest
-    from tests import TestGraph
+    from tests import TestGraph  # Import test
 
     unittest.main()  # Run Unit tests
