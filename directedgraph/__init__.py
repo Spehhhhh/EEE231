534ae7b43cab191c63707e84d3493cf4e57db80b
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-10 20:28:34 +0000

    fix test

--- directedgraph/__init__.py
+++ directedgraph/__init__.py
@@ -1,9 +1,3 @@
 from pathlib import Path
 
 print("Running" if __name__ == "__main__" else "Importing", Path(__file__).resolve())
-
-from directedgraph.dggui import GraphEditor
-
-if __name__ == "__main__":
-    app = GraphEditor()
-    app.run()
